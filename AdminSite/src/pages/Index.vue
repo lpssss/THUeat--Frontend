c399831e53d9cf93e22a429b53ac2c88cb979387
Deon Chon <deon.chon@gmail.com>
2021-10-21 13:31:15 +0800

    Structure Setup

--- AdminSite/src/pages/Index.vue
+++ AdminSite/src/pages/Index.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <p>Login Page</p>
   </q-page>
 </template>
 
