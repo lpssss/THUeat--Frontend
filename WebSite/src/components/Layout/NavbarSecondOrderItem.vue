ada5b3098a7617ca9c65925a04d8ae6bcc5c7a91
lpssss <lim55570@gmail.com>
2021-10-26 22:30:35 +0800

    feat: Add canteen and layout page
    4 Components for canteen page (Title, BasicDetail, Intro, Stall).
    2 Components for layout page (FirstOrderItem, SecondOrderItem).

---
+++ WebSite/src/components/Layout/NavbarSecondOrderItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-item clickable v-ripple>
+    <q-item-section>
+      <div class="row justify-center">
+        <div>{{title}}</div>
+      </div>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: "NavbarSecondOrderItem",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
