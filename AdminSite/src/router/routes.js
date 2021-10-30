d7b917bbd2e62be50102d5897598d62585517994
Deon Chon <deon.chon@gmail.com>
2021-10-30 15:14:12 +0800

    Change file name

--- AdminSite/src/router/routes.js
+++ AdminSite/src/router/routes.js
@@ -1,10 +1,9 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Login.vue') }
     ]
   },
 
