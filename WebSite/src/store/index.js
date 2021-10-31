0b2b949f6ce95154edde7caaae447fc9b88329f1
lpssss <lim55570@gmail.com>
2021-10-31 20:26:00 +0800

    feat: Add Login Store in Index, Delete redundant store example

--- WebSite/src/store/index.js
+++ WebSite/src/store/index.js
@@ -1,5 +1,6 @@
-import { store } from 'quasar/wrappers'
-import { createStore } from 'vuex'
+import { store } from "quasar/wrappers";
+import { createStore } from "vuex";
+import login from "./login";
 
 // import example from './module-example'
 
@@ -16,12 +17,13 @@ export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
       // example
+      login,
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
-    strict: process.env.DEBUGGING
-  })
+    strict: process.env.DEBUGGING,
+  });
 
-  return Store
-})
+  return Store;
+});
