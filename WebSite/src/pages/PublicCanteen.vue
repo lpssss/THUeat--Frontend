1eb233d5e9e76f73245e7921608327d80b01b53d
leeasong <1249103659@qq.com>
2021-11-02 22:41:35 +0800

    test

--- WebSite/src/pages/PublicCanteen.vue
+++ WebSite/src/pages/PublicCanteen.vue
@@ -31,6 +31,8 @@ import CanteenBasicDetailSection from "components/CanteenPage/CanteenBasicDetail
 import CanteenIntroSection from "components/CanteenPage/CanteenIntroSection";
 import CanteenTitleSection from "components/CanteenPage/CanteenTitleSection";
 import CanteenStallSection from "components/CanteenPage/CanteenStallSection";
+import { watch, watchEffect } from 'vue';
+import {useRoute} from 'vue-router'
 
 export default defineComponent({
   name: "PublicCanteen",
@@ -51,6 +53,7 @@ export default defineComponent({
         console.log(err.message);
       }
     };
+
     getCanteenData();
 
     return {
