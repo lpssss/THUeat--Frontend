d1ea1bab9e16b221ce08fd39986618369cb6ecec
lpssss <lim55570@gmail.com>
2021-11-07 09:36:51 +0800

    feat: Add dish page
    Components reused: StallPicture, JudgeCard
    Code reused: Content part from StallIntro, Thumbs up from dishCard

--- WebSite/src/pages/Dish.vue
+++ WebSite/src/pages/Dish.vue
@@ -1,16 +1,60 @@
 <template>
-  <div>Dishpage</div>
+  <template v-if="Object.keys(dishData).length">
+  <StallPictureSection
+    :img-src="dishData.data.imgSrc"
+    :stall-name="dishData.data.dishName"
+  />
+  <q-card flat bordered class="bg-purple-1" style="width:100%">
+    <q-card-section class="text-grey-9">
+      {{dishData.data.comment}}
+    </q-card-section>
+    <q-separator  inset />
+    <q-card-section class="q-pt-sm">
+      <q-btn size="sm" falt round color="primary" icon="thumb_up" />
+      <span class="q-px-sm text-caption text-grey"> {{ dishData.data.thumb }} </span>
+    </q-card-section>
+  </q-card>
+    <div class="q-pa-md">
+    <div class="text-h6">用餐者评价</div>
+    <div class="q-pa-md row items-start q-gutter-md justify-center col-md-4">
+      <JudgeCardSection
+        v-for="judgement in dishData.data.judgements"
+        v-bind="judgement"
+        :key="judgement.title"
+      />
+    </div>
+    </div>
+  </template>
 </template>
 
 <script>
+import { reactive } from "vue";
+import axios from "axios";
+import StallPictureSection from "components/StallPage/StallPictureSection";
+import JudgeCardSection from "components/StallPage/JudgeCardSection";
+
 export default {
   name: "Dish",
-  setup(){
+  // Components需改名
+  components: {JudgeCardSection, StallPictureSection },
+  setup() {
+    const API_LINK = "http://localhost:3000/dish"; // 之后放真正的API
+    const dishData = reactive({ data: {} });
+    const getDishData = async () => {
+      try {
+        const response = await axios.get(API_LINK);
+        dishData.data = response.data[0];
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getDishData();
 
-  }
-}
+    return {
+      dishData,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
