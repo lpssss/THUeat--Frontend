97d1e0b21b9c19f857621c7d49aad2df93ff1be9
lpssss <lim55570@gmail.com>
2021-11-16 19:10:50 +0800

    feat: Add Stall Details Table to display stall-related data for stall admin
    Table includes ALL stall-related data, some will be read-only, some will be modifiable

---
+++ AdminSite/src/components/StallDetailsTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="q-pa-md">
+    <q-table title="Treats" :rows="rows" :columns="columns" row-key="name" />
+  </div>
+</template>
+
+<script>
+const columns = [
+  {
+    name: "attributeName",
+    required: true,
+    label: "档口基本信息",
+    align: "left",
+    field: (row) => row.attributeName,
+  },
+  {
+    name: "attribute",
+    align: "center",
+    label: "属性",
+    field: "attribute",
+  },
+];
+
+export default {
+  name: "StallDetailsTable",
+  props:{
+    // rows的部分需要从api获取的数据整理
+    rows:{
+      type:Array,
+      required:true
+    }
+  },
+  setup() {
+    return {
+      columns,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
