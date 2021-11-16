f3a7bb78bd8a04d46b80382111eb3890bc6f957f
lpssss <lim55570@gmail.com>
2021-11-16 18:50:10 +0800

    feat: Add Dashboard Card to display data for admin
    Dashboard Card accepts 3 props: titleName, iconName, displayedData

---
+++ AdminSite/src/components/DashboardCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-12 col-md">
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6" style="border-bottom: 1px solid; font-weight: bold;">
+          {{titleName}}
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pt-none text-h4" style="text-align: center;">
+        <q-icon :name="iconName"/>
+        {{displayedData}}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardCard",
+  props:{
+    titleName:{
+      type:String,
+      required:true
+    },
+    iconName:{
+      type:String,
+      required:true
+    },
+    displayedData:{
+      type:String,
+      required:true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
