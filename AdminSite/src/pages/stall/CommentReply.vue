b069496e67827d8d46326775e57365df295f6d79
lpssss <lim55570@gmail.com>
2021-11-23 18:28:29 +0800

    feat: Add CommentReply and DishManagement Page (still under construction)

---
+++ AdminSite/src/pages/stall/CommentReply.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "CommentReply"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ AdminSite/src/pages/stall/DishManagement.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DishManagement"
+}
+</script>
+
+<style scoped>
+
+</style>
