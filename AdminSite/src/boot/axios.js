fe0a47b1cc772eca4eeef2c69b3f96362ce2fa81
Deon Chon <deon.chon@gmail.com>
2021-11-30 18:47:59 +0800

    add global get token function

--- AdminSite/src/boot/axios.js
+++ AdminSite/src/boot/axios.js
@@ -1,5 +1,9 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
+import useAppState from "src/store/userAppState.js";
+
+
+const { getToken } = useAppState();
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -12,8 +16,18 @@ const api = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  const token = getToken().value;
+
+  if (token !== null) {
+    api.interceptors.request.use(async (req) => {
+      req.headers.Authorization = `token ${token}`;
+      return req;
+    });
+  }
 
   app.config.globalProperties.$axios = axios;
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
