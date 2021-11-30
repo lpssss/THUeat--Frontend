8788a942e18845548bd5d62861b7b9c206aad48c
Deon Chon <deon.chon@gmail.com>
2021-11-30 13:39:30 +0800

    add get and post function

--- AdminSite/src/pages/school/stallCreation.vue
+++ AdminSite/src/pages/school/stallCreation.vue
@@ -1,107 +1,200 @@
 <template>
-  <div class="q-pa-md" v-if="creatable == false">
+  <div
+    class="q-pa-md"
+    v-if="creatable == false"
+  >
     <q-table
       title="档口创建系统"
-      :rows="data"
+      :rows="stalls"
       :columns="columns"
       row-key="name"
       binary-state-sort
     >
-    <template v-slot:top-right>
-      <q-btn color="primary" @click="addAdv">创建</q-btn>
-    </template>
-
-      <template v-slot:body-cell-status="props">
-        <q-td :props="props">
-          <q-toggle v-model="props.row.status" checked-icon="check" color="green" unchecked-icon="clear" />
-        </q-td>
+      <template v-slot:top-right>
+        <q-btn color="primary" @click="add">创建</q-btn>
       </template>
 
-      <template v-slot:body-cell-btnDelete="props">
+           <template v-slot:body-cell-status="props">
         <q-td :props="props">
-            <q-btn size="10px" outline round color="red" icon="close" />
+          <q-toggle v-model="props.row.stallStatus" 
+                    checked-icon="check" 
+                    color="green" 
+                    unchecked-icon="clear"
+                    @update:model-value="
+                      (...args) => {
+                    doSomething(props.row, ...args);}
+              "  />
         </q-td>
       </template>
-
     </q-table>
   </div>
 
-  <div class="q-pa-md" style="margin-left:10%; margin-right:10%;" v-if="creatable == true">
+  <q-dialog v-model="confirmChangeStatus" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <span class="q-ml-sm">您是否确定更改状态？</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="取消" color="primary" @click="postStatusChange" v-close-popup />
+          <q-btn
+            flat
+            label="确定"
+            color="primary"
+            @click="postStatusChange"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+  <div
+    class="q-pa-md"
+    style="margin-left:10%; margin-right:10%;"
+    v-if="creatable == true"
+  >
     <h4 style="border-bottom: 0.1px solid;">档口创建</h4>
 
-    <div class="q-gutter-md" style="max-width: 100%">
-      <q-select filled v-model="model" :options="options" label="食堂" />
-      <q-select filled v-model="model" :options="options" label="楼层" />
-      <q-select filled v-model="model" :options="options" label="档口" />
-      <q-input filled v-model="text" label="档口名字" stack-label />
+    <div
+      class="q-gutter-md"
+      style="max-width: 100%"
+    >
+      <q-select filled v-model="newStall.canteenName" :options="canteen" label="食堂" />
+      <q-select filled v-model="newStall.stallFloor" :options="floor" label="楼层" />
+      <q-input  filled v-model="newStall.stallName" label="档口名字" stack-label />
       <div style="margin-left:45%;">
-        <q-btn style="margin-right:10px;" color="red" @click="saveAdv()">取消</q-btn>
-        <q-btn color="green" @click="saveAdv()">创建</q-btn>   
-      </div>   
+        <q-btn style="margin-right:10px;" color="red" @click="cancel()">取消</q-btn>
+        <q-btn
+          color="green"
+          @click="save()"
+        >创建</q-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive, onMounted } from 'vue'
+// import { api } from 'boot/axios'
 
 const columns = [
-  {
-    name: 'stallName',
-    required: true,
-    label: '档口名字',
-    align: 'left',
-    field: row => row.stallName,
-    format: val => `${val}`,
-    sortable: true
-  },
+  { name: 'stallName', label: '档口名字', align: 'left', field:'stallName', sortable: true },
   { name: 'canteenName', align: 'left', label: '食堂名字', field: 'canteenName', sortable: true },
-  { name: 'canteenLevel', align: 'left', label: '楼层', field: 'canteenLevel', sortable: true },
-  { name: 'status', align: 'left', label: '激活状态', field: 'status', sortable: true },
+  { name: 'stallFloor', align: 'left', label: '楼层', field: 'stallFloor', sortable: true },
+  { name: 'status', align: 'left', label: '激活状态', field: 'stallStatus', sortable: true }
 ]
 
-const data = [
- {
+const stalls = [
+  {
     stallName: '麻辣香锅',
     canteenName: '紫荆园',
-    canteenLevel: '第一层',
-    status: false
+    stallFloor: 1,
+    stallStatus: false
   },
   {
     stallName: '麻辣香锅',
     canteenName: '桃李园',
-    canteenLevel: '第二层',
-    status: true
+    stallFloor: 2,
+    stallStatus: true
   },
   {
     stallName: '鸡饭档',
     canteenName: '桃李园',
-    canteenLevel: '第三层',
-    status: true
+    stallFloor: 3,
+    stallStatus: true
   }
 ]
 
 export default defineComponent({
   setup () {
     const creatable = ref(false);
-    const addAdv = () => {
-        console.log("test");
-        creatable.value = true
+    const orgStall = {
+        stallName: null,
+        canteenName: null,
+        stallFloor: null
+      };
+
+    const newStall = reactive({...orgStall});
+    const add = () => {
+      creatable.value = true;
+      Object.assign(newStall, orgStall)
     };
-    const saveAdv = () => {
-        creatable.value = false
+    const cancel = () => {
+      creatable.value = false
     };
+    const save = () => {
+      creatable.value = false
+    };
+
+    const confirmChangeStatus = ref(false);
+    const postStatusChange = () => {
+      console.log("confirm");
+    };
+    function doSomething(a, value, evt) {
+      a.dishStatus = value;
+      confirmChangeStatus.value = true;
+    };
+
+    // const stalls = ref([]);
+    // const getstallsData = async () => {
+    //   try {
+    //     const response = await api.get("link");
+    //     stalls.value.splice(0, stalls.value.length, ...response.data);
+    //   } catch (err) {
+    //     console.log(err.message);
+    //   }
+    // }
+
+    // const postNewStall = () => {
+    // api.post('Api_Link', {
+    //   stallName: newStall.stallName.value,
+    //   stallFloor: newStall.stallFloor.value, 
+    //   canteenId: newStall.canteenId.value，
+    // }).then ((res) => {
+      // if (res.status === 200 && res.data !== undefined) {
+      //     updateToken(res.data.token);
+      // }
+      // if (res.status === 404){
+      //   console.log('error')
+      // }
+    //  
+    // })
+
+    // const postStallStatus = () => {
+    // api.post('Api_Link', {
+    //   stallStatus: stalls.stallStatus.value,
+    // }).then ((res) => {
+      // if (res.status === 200 && res.data !== undefined) {
+      //     updateToken(res.data.token);
+      // }
+      // if (res.status === 404){
+      //   console.log('error')
+      // }
+    //  
+    // })
+
+    // onMounted(() => {
+    //   getstallsData()
+    // })
+
     return {
       columns,
-      data: ref(data),
-      text: ref(''),
-      model: ref(null),
-      options: [
+      orgStall,
+      newStall,
+      stalls: ref(stalls),
+      canteen: [
         '桃李园', '紫荆园', '桃李园', '万人食堂'
       ],
+      floor: [
+        1,2,3,4
+      ],
       creatable,
-      addAdv,
-      saveAdv,
+      confirmChangeStatus,
+      postStatusChange,
+      doSomething,
+      add,
+      cancel,
+      save,
     }
   }
 })
