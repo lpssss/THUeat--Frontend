c98ee187c0a676c23da57c7ccf4c9144dc5a97fe
Deon Chon <deon.chon@gmail.com>
2021-11-30 13:39:14 +0800

    add get and post function

--- AdminSite/src/pages/school/staffMaintenance.vue
+++ AdminSite/src/pages/school/staffMaintenance.vue
@@ -2,118 +2,210 @@
   <div class="q-pa-md" v-if="editable == false">
     <q-table
       title="档主管理系统"
-      :rows="data"
+      :rows="staffs"
       :columns="columns"
       row-key="name"
       binary-state-sort
     >
     <template v-slot:top-right>
-      <q-btn color="primary" @click="addAdv">创建</q-btn>
+      <q-btn color="primary" @click="add">创建</q-btn>
     </template>
 
       <template v-slot:body-cell-status="props">
         <q-td :props="props">
-          <q-toggle v-model="props.row.status" checked-icon="check" color="green" unchecked-icon="clear" />
+          <q-toggle v-model="props.row.staffStatus" 
+                    checked-icon="check" 
+                    color="green" 
+                    unchecked-icon="clear" 
+                    @update:model-value="
+                      (...args) => {
+                    doSomething(props.row, ...args);}
+              " />
         </q-td>
       </template>
-
-      <template v-slot:body-cell-btnDelete="props">
-        <q-td :props="props">
-            <q-btn size="10px" outline round color="red" icon="close" />
-        </q-td>
-      </template>
-
     </q-table>
   </div>
+
+  <q-dialog v-model="confirmChangeStatus" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <span class="q-ml-sm">您是否确定更改状态？</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="取消" color="primary" @click="postStatusChange" v-close-popup />
+          <q-btn
+            flat
+            label="确定"
+            color="primary"
+            @click="postStatusChange"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
   <div class="q-pa-md" style="margin-left:10%; margin-right:10%;" v-if="editable == true">
     <h4 style="border-bottom: 0.1px solid;">档主创建</h4>
 
     <div class="q-gutter-md" style="max-width: 100%">
-      <q-input filled v-model="text" label="档主姓名" stack-label />
-      <q-input filled v-model="text" label="档主联络号码" stack-label />
-      <q-select filled v-model="model" :options="options" label="食堂" />
-      <q-select filled v-model="model" :options="options" label="楼层" />
-      <q-select filled v-model="model" :options="options" label="档口" />
+      <q-input filled v-model="newStaff.staffVaildName" label="档主姓名" stack-label />
+      <q-input filled v-model="newStaff.staffPhone" label="档主联络号码" stack-label />
+      <q-select filled v-model="newStaff.canteenName" :options="canteen" label="食堂" />
+      <q-select filled v-model="newStaff.stallFloor" :options="floor" label="楼层" />
+      <q-select filled v-model="newStaff.stallName" :options="stall" label="档口" />
       <div style="margin-left:45%;">
-        <q-btn style="margin-right:10px;" color="red" @click="saveAdv()">取消</q-btn>
-        <q-btn color="green" @click="saveAdv()">创建</q-btn>   
+        <q-btn style="margin-right:10px;" color="red" @click="cancel()">取消</q-btn>
+        <q-btn color="green" @click="save()">创建</q-btn>   
       </div>   
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
+// import { api } from 'boot/axios'
 
 const columns = [
-  {
-    name: 'Username',
-    required: true,
-    label: '档主姓名',
-    align: 'left',
-    field: row => row.Username,
-    format: val => `${val}`,
-    sortable: true
-  },
-  { name: 'Staff_ID', align: 'left', label: '职员编号', field: 'Staff_ID', sortable: true },
+  { name: 'staffVaildName', align: 'left', label: '档主姓名', field: 'staffVaildName', sortable: true },
+  { name: 'staffID', align: 'left', label: '职员编号', field: 'staffID', sortable: true },
   { name: 'canteenName', align: 'left', label: '食堂名字', field: 'canteenName', sortable: true },
-  { name: 'canteenLevel', align: 'left', label: '楼层', field: 'canteenLevel', sortable: true },
+  { name: 'stallFloor', align: 'left', label: '楼层', field: 'stallFloor', sortable: true },
   { name: 'stallName', align: 'left', label: '档口名字', field: 'stallName', sortable: true },
-  { name: 'stallEmail', align: 'left', label: '联络号码', field: 'stallEmail' },
-  { name: 'status', align: 'left', label: '激活状态', field: 'status', sortable: true },
+  { name: 'staffPhone', align: 'left', label: '联络号码', field: 'staffPhone' },
+  { name: 'status', align: 'left', label: '激活状态', field: 'staffStatus', sortable: true },
 ]
 
-const data = [
+const staffs = [
   {
-    Username: '李先生',
-    Staff_ID: 'W0001',
+    staffVaildName: '李先生',
+    staffID: 'W0001',
     stallName: '麻辣香锅档',
     canteenName: '紫荆园',
-    canteenLevel: '第一层',
-    stallEmail: 'test1@mail.com',
-    status: false
+    stallFloor: 1,
+    staffPhone: '12345',
+    staffStatus: false
   },
   {
-    Username: '黄女士',
-    Staff_ID: 'W0002',
+    staffVaildName: '黄女士',
+    staffID: 'W0002',
     canteenName: '桃李园',
     stallName: '麻辣香锅档',
-    canteenLevel: '第二层',
-    stallEmail: 'test2@mail.com',
-    status: true
+    stallFloor: 2,
+    staffPhone: '12345',
+    staffStatus: true
   },
   {
-    Username: '李先生',
-    Staff_ID: 'W0003',
+    staffVaildName: '李先生',
+    staffID: 'W0003',
     stallName: '鸡饭档',
     canteenName: '桃李园',
-    canteenLevel: '第三层',
-    stallEmail: 'test3@mail.com',
-    status: true
+    stallFloor: 3,
+    staffPhone: '12345',
+    staffStatus: true
   }
 ]
 
 export default defineComponent({
   setup () {
     const editable = ref(false);
-    const addAdv = () => {
-        editable.value = true
+    const orgStaff = {
+        staffVaildName: null,
+        staffPhone: null,
+        canteenName: null,
+        stallFloor: null,
+        stallName: null
+      };
+
+    const newStaff = reactive({...orgStaff});
+
+    const add = () => {
+        editable.value = true;
+        Object.assign(newStaff, orgStaff)
     };
-    const saveAdv = () => {
+    const cancel = () => {
         editable.value = false
     };
+    const save = () => {
+        editable.value = false
+    };
+
+    const confirmChangeStatus = ref(false);
+    const postStatusChange = () => {
+      console.log("confirm");
+    };
+    function doSomething(a, value, evt) {
+      a.dishStatus = value;
+      confirmChangeStatus.value = true;
+    }
+
+    // const staffs = ref([]);
+    // const getstaffsData = async () => {
+    //   try {
+    //     const response = await api.get("link");
+    //     staffs.value.splice(0, staffs.value.length, ...response.data);
+    //   } catch (err) {
+    //     console.log(err.message);
+    //   }
+    // }
+
+    // const postNewStaff = () => {
+    // api.post('Api_Link', {
+    //   staffVaildName: newStaff.staffVaildName.value,
+    //   staffPhone: newStaff.staffPhone.value, 
+    //   canteenId: newStaff.canteenId.value，
+    //   stallFloor: newStaff.stallFloor.value，
+    //   stallId: newStaff.stallId.value，
+
+    // }).then ((res) => {
+      // if (res.status === 200 && res.data !== undefined) {
+      //     updateToken(res.data.token);
+      // }
+      // if (res.status === 404){
+      //   console.log('error')
+      // }
+    //  
+    // })
+
+    // const postStaffStatus = () => {
+    // api.post('Api_Link', {
+    //   staffStatus: staffs.staffStatus.value,
+    // }).then ((res) => {
+      // if (res.status === 200 && res.data !== undefined) {
+      //     updateToken(res.data.token);
+      // }
+      // if (res.status === 404){
+      //   console.log('error')
+      // }
+    //  
+    // })
+
+    // onMounted(() => {
+    //   getstaffsData()
+    // })
+  
     return {
       columns,
-      data: ref(data),
-      text: ref(''),
+      staffs: ref(staffs),
+      orgStaff,
+      newStaff,
       model: ref(null),
-      options: [
+      canteen: [
         '桃李园', '紫荆园', '桃李园', '万人食堂'
       ],
+      floor: [
+        1,2,3,4
+      ],
+      stall: [
+        '档口1', '档口2', '档口3'
+      ],
       editable,
-      addAdv,
-      saveAdv,
+      confirmChangeStatus,
+      postStatusChange,
+      doSomething,
+      add,
+      cancel,
+      save,
     }
   }
 })
