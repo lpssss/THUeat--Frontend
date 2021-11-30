3ba7366219554ff5fc3080b92817eccb5460ffce
Deon Chon <deon.chon@gmail.com>
2021-11-30 18:50:42 +0800

    add login authenticate function

--- AdminSite/src/pages/Login.vue
+++ AdminSite/src/pages/Login.vue
@@ -8,12 +8,10 @@
       <q-card class="login-card">
         <h6 align="center">乐吃管理系统</h6>
         <div class="q-pa-md">
-          <q-form
-            @submit="onSubmit"
-            class="q-gutter-md"
-          >
+          <!-- <q-form class="q-gutter-md"> -->
+          <div class="q-gutter-md">
             <q-input
-              v-model="nickname"
+              v-model="name"
               label="用户名"
               lazy-rules
               :rules="[(val) => (val && val.length > 0) || '用户名不能为空']"
@@ -45,70 +43,160 @@
                 <q-btn
                   align="right"
                   label="登录"
-                  type="submit"
                   color="primary"
-                  :loading="onsubmit"
+                  @click="onLogin"
                 />
               </div>
             </div>
-          </q-form>
+          </div>
         </div>
       </q-card>
     </q-page>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      nickname: null,
-      password: null,
-      isPwd: true,
-      acceptclause: false,
-      onsubmit: false,
-    };
-  },
 
-  methods: {
-    onSubmit () {
-      if (this.acceptclause !== true) {
-        this.$q.notify({
+<script>
+import { defineComponent, ref } from 'vue';
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
+import useAppState from "src/store/userAppState.js";
+import { api } from 'boot/axios'
+
+
+const { updateToken, updateFirstLogin, updateType } = useAppState();
+
+
+export default defineComponent({
+  setup () {
+    const router = useRouter();
+    const $q = useQuasar();
+    const name = ref('用户名');
+    const password = ref('123456');
+    const isPwd = ref(true);
+    const acceptclause = ref(true);
+
+
+    const login = async () => {
+      const res = await api.get("/user");
+      await api.get('/user').then((res) => {
+        if (res.status === 200 && res.data !== undefined) {
+          updateToken(res.data.token);
+          updateType(res.data.type);
+          if (res.data.firstLogin === true) {
+
+            console.log('first')
+            // Route to First login page
+            router.push("/firstLoginSettings");
+          }
+          else {
+            // Route to normal page
+            switch (res.data.type) {
+              case "super":
+                router.push('/superadmin');
+                break;
+              case "admin":
+                router.push('/admin');
+                break;
+              case "staff":
+                router.push('/staff');
+                break;
+              default:
+                router.push('/');
+            }
+            $q.notify({
+              color: "green-4",
+              textColor: "white",
+              icon: "cloud_done",
+              message: "登录成功",
+              timeout: 500,
+            });
+          }
+        }
+      })
+    }
+
+
+
+    const onLogin = () => {
+      if (acceptclause.value !== true) {
+        $q.notify({
           color: "red-5",
           textColor: "white",
           icon: "warning",
           message: "请接受条款",
           timeout: 500,
         });
-      } else {
-        this.onsubmit = true;
-        let data = {
-          nickname: this.nickname,
-          password: this.password,
-        };
-        /* this.$http.post("http://localhost:3000/users", data)
-        .then((req) => {
-
-          console.log(req);
-        })
-        .catch(err=>{
-          console.log(err);
-        }); */
-        //this.$store.dispatch('user/asyncSetuserInfo',req.data.token)
-        this.onsubmit = false;
-        this.$q.notify({
-          color: "green-4",
-          textColor: "white",
-          icon: "cloud_done",
-          message: "登录成功",
-          timeout: 500,
-        });
-        // this.$store.commit("login/updateLoginStatus", true);
-        // this.$router.push({ name: "Index" }); //编程式导航
       }
-    },
-  },
-};
+      if (name.value.length <= 0) {
+        $q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: "请填些用户名",
+          timeout: 500
+        });
+      }
+      else {
+        login()
+
+
+
+
+
+        // const login = async () => {
+        //   console.log('b');
+
+        //   const res = await api.get("/user");
+        //   console.log(res);
+
+        // await api.get('/user').then((res) => {
+
+
+
+        //   if (res.status === 200 && res.data !== undefined) {
+        //     updateToken(res.data.token);
+        //     updateType(res.data.type);
+        //     if (res.data.firstLogin === true) {
+        //       // Route to First login page
+        //       router.push({ path: 'firstLoginSettings' })
+        //     }
+        //     else {
+        //       // Route to normal page
+        //       switch (res.data.type) {
+        //         case "super":
+        //           // code block
+        //           router.push({ path: 'superadmin' });
+        //           break;
+        //         case "admin":
+        //           // code block
+        //           router.push({ path: 'admin' });
+        //           break;
+        //         case "staff":
+        //           // code block
+        //           router.push({ path: 'staff' });
+        //           break;
+        //         default:
+        //           // code block
+        //           router.push({ path: '/' });
+        //       }
+        //       // this.$q.notify({
+        //       //   color: "green-4",
+        //       //   textColor: "white",
+        //       //   icon: "cloud_done",
+        //       //   message: "登录成功",
+        //       //   timeout: 500,
+        //       // });
+        //     }
+        //   }
+        // })
+        //}
+      }
+    }
+    return { name, password, isPwd, acceptclause, onLogin }
+  }
+})
+
 </script>
 
 <style lang="sass" scoped>
