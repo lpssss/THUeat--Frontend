e28a7e935ca97c270db5d3909ae972ab2f501273
Deon Chon <deon.chon@gmail.com>
2021-12-04 19:29:34 +0800

    fix firstloginsettings function

--- AdminSite/src/pages/firstLogin.vue
+++ AdminSite/src/pages/firstLogin.vue
@@ -10,37 +10,25 @@
     >
       <q-input
         filled
-        label="姓名"
-        disable
-        v-model="name"
-      />
-      <q-input
-        filled
-        label="编号"
-        disable
-        v-model="id"
-      />
-      <q-input
-        filled
         label="用户名"
-        disable
-        v-model="id"
+        v-model="name"
         lazy-rules
-        :rules="[(val) => (val && val.length > 0) || '联络号码不能为空']"
+        :rules="[(val) => (val && val.length > 0) || '用户名不能为空']"
       />
       <q-input
         filled
-        label="联络号码"
-        stack-label
-        v-model="contact"
+        label="输入旧密码"
+        v-model="oldPassword"
+        type="password"
         lazy-rules
-        :rules="[(val) => (val && val.length > 0) || '联络号码不能为空']"
+        :rules="[(val) => (val && val.length >= 6) || '密码不能少于6位']"
       />
       <q-input
         filled
         label="输入新密码"
-        v-model="newPassword"
+        v-model="password"
         type="password"
+        lazy-rules
         :rules="[(val) => (val && val.length >= 6) || '密码不能少于6位']"
       />
       <q-input
@@ -48,7 +36,8 @@
         label="确定新密码"
         v-model="confirmPassword"
         type="password"
-        :rules="[(val) => (val && val.length >= 0) || '确定密码不可为空']"
+        lazy-rules
+        :rules="[(val) => (val && val.length >= 6) || '确定密码不可为空']"
       />
 
       <div style="margin-left:50%;">
@@ -63,32 +52,67 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
+import useAppState from "src/store/userAppState.js";
+
+const { resetState } = useAppState();
 
 export default defineComponent({
   setup () {
-    const name = ref('庄家菘');
-    const id = ref('123456');
-    const contact = ref('1881332025');
-    const newPassword = ref();
-    const confirmPassword = ref()
+    const router = useRouter();
+    const $q = useQuasar();
+    const name = ref('庄家菘1');
+    const oldPassword = ref('');
+    const password = ref('');
+    const confirmPassword = ref('')
 
-    const save = () => {
-      if ((newPassword.value == confirmPassword.value)) {
-        console.log("pass");
-        console.log(newPassword.value)
+    //检查用户输入资料是否合格
+    const checkInput = () => {
+      if (name.value.length <= 0) {
+        $q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: "用户名不能为空",
+          timeout: 1000,
+        });
       }
-      else {
-        console.log("password incorrect")
-        console.log(newPassword.value)
+      if ((oldPassword.value.length < 6 || password.value.length < 6 || confirmPassword.value.length < 6)) {
+        $q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: "密码不能小于6位数",
+          timeout: 1000,
+        });
+      }
+      if ((password.value !== confirmPassword.value)) {
+        $q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: "新密码与确定密码不相等",
+          timeout: 1000,
+        });
+      }
+    }
+
+    //更新用户资料
+    const save = () => {
+      checkInput();
+      if ((password.value == confirmPassword.value) && (password.value.length > 5) && (name.value.length > 0)) {
+        resetState();
+        router.push('/');
       }
     };
     return {
-      newPassword,
+      password,
+      oldPassword,
       confirmPassword,
+      checkInput,
       save,
-      name,
-      id,
-      contact
+      name
     }
   }
 })
