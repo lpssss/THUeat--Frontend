c540268d6a496ba010fd23cb51579e0e362bc96f
Deon Chon <deon.chon@gmail.com>
2021-12-04 19:27:22 +0800

    fix firstloginsettings function

--- AdminSite/src/router/routes.js
+++ AdminSite/src/router/routes.js
@@ -1,16 +1,10 @@
-import useAppState from "src/store/userAppState.js";
-
-const { getType, getFirstLogin } = useAppState();
-const userType = getType().value;
-//const isFirstLogin = getFirstLogin().value;
-
 const routes = [
   {
     path: "/",
     component: () => import("src/layouts/LoginLayout.vue"),
     children: [
       { name: "login", path: "", component: () => import("pages/Login.vue"), alias: "/login" },
-      { path: "firstLoginSettings", component: () => import("pages/firstLogin.vue") },
+      { name: "firstLoginSettings", path: "firstLoginSettings", component: () => import("pages/firstLogin.vue") },
 
     ],
   },
@@ -156,100 +150,3 @@ const routes = [
 ];
 
 export default routes;
-
-// const routes = [
-//   {
-//     path: "/",
-//     component: () => import("src/layouts/LoginLayout.vue"),
-//     children: [
-//       { path: "", component: () => import("pages/Login.vue"), alias: "/login" },
-//       { path: "firstLoginSettings", component: () => import("pages/firstLogin.vue") },
-
-//     ],
-//   },
-
-//   {
-//     path: "/superadmin/",
-//     component: () => import("src/layouts/superadminLayout.vue"),
-//     children: [
-//       { path: "", component: () => import("pages/school/schoolDashboard.vue") },
-//       {
-//         path: "schooladminMaintenance",
-//         component: () => import("pages/school/schooladminMaintenance.vue"),
-//       },
-//       {
-//         path: "stallCreation",
-//         component: () => import("pages/school/stallCreation.vue"),
-//       },
-//       {
-//         path: "staffMaintenance",
-//         component: () => import("pages/school/staffMaintenance.vue"),
-//       },
-//       {
-//         path: "userMaintenance",
-//         component: () => import("pages/school/userMaintenance.vue"),
-//       },
-//       {
-//         path: "settings",
-//         component: () => import("pages/school/settings.vue"),
-//       },
-//     ],
-//   },
-//   {
-//     path: "/admin/",
-//     component: () => import("src/layouts/adminLayout.vue"),
-//     children: [
-//       {
-//         path: "",
-//         component: () => import("pages/school/schoolDashboard.vue"),
-//       },
-//       {
-//         path: "stallCreation",
-//         component: () => import("pages/school/stallCreation.vue"),
-//       },
-//       {
-//         path: "staffMaintenance",
-//         component: () => import("pages/school/staffMaintenance.vue"),
-//       },
-//       {
-//         path: "userMaintenance",
-//         component: () => import("pages/school/userMaintenance.vue"),
-//       },
-//       {
-//         path: "settings",
-//         component: () => import("pages/school/settings.vue"),
-//       },
-//     ],
-//   },
-//   {
-//     path: "/staff/",
-//     component: () => import("layouts/StaffLayout.vue"),
-//     children: [
-//       {
-//         path: "",
-//         component: () => import("pages/stall/StallDashboard.vue"),
-//       },
-//       {
-//         path: "reviews",
-//         component: () => import("pages/stall/StallReviews.vue"),
-//       },
-//       {
-//         path: "dishes",
-//         component: () => import("pages/stall/DishManagement.vue"),
-//       },
-//       {
-//         path: "settings",
-//         component: () => import("pages/stall/StaffSettings.vue"),
-//       },
-//     ],
-//   },
-
-//   // Always leave this as last one,
-//   // but you can also remove it
-//   {
-//     path: "/:catchAll(.*)*",
-//     component: () => import("pages/Error404.vue"),
-//   },
-// ];
-
-// export default routes;
