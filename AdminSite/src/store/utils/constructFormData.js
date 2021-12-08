30245600fef2663e420e365a928ca7a711f218de
lpssss <lim55570@gmail.com>
2021-12-08 15:54:33 +0800

    Make form data script to work for dish create (form)

--- AdminSite/src/store/utils/constructFormData.js
+++ AdminSite/src/store/utils/constructFormData.js
@@ -1,17 +1,25 @@
-export default function (nDetails, dImage, nImage) {
+export default function (
+  nDetails,
+  dImage,
+  nImage,
+  imageType,
+  isDishForm = false
+) {
   let formData = new FormData();
   for (let key in nDetails) {
     formData.append(key, nDetails[key]);
   }
-  if (dImage !== null && dImage.length !== 0) {
-    dImage.forEach((item) => formData.append("deleteImages", item));
-  } else {
-    formData.append("deleteImages", "");
+  if (!isDishForm) {
+    if (dImage !== null && dImage.length !== 0) {
+      dImage.forEach((item) => formData.append("deleteImages", item));
+    } else {
+      formData.append("deleteImages", "");
+    }
   }
   if (nImage !== null && nImage.length !== 0) {
-    nImage.forEach((item) => formData.append("stallImages", item));
+    nImage.forEach((item) => formData.append(imageType, item));
   } else {
-    formData.append("stallImages", "");
+    formData.append(imageType, "");
   }
   return formData;
 }
