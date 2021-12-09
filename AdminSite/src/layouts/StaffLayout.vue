3b159aa2ff9e84c8310afe62f2a171f3976f031b
Deon Chon <deon.chon@gmail.com>
2021-12-09 13:16:41 +0800

    add username at layout

--- AdminSite/src/layouts/StaffLayout.vue
+++ AdminSite/src/layouts/StaffLayout.vue
@@ -15,7 +15,7 @@
           乐吃 - 档口管理系统
         </q-toolbar-title>
 
-        <q-chip>用户名</q-chip>
+        <q-chip>{{ name }}</q-chip>
         <q-btn
           color="red"
           @click="logout"
@@ -52,6 +52,10 @@ import { defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 import useAppState from "src/store/userAppState.js";
 import { useRouter } from "vue-router";
+
+
+const { getName } = useAppState();
+const name = getName();
 
 const linksList = [
   {
@@ -91,6 +95,7 @@ export default defineComponent({
       router.push('/');
     }
     return {
+      name,
       essentialLinks: linksList,
       leftDrawerOpen,
       logout,
