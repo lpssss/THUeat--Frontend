b9ef57df4e0615ea5e30c9a32ed982606122e35a
lpssss <lim55570@gmail.com>
2021-12-17 13:00:39 +0800

    Fix notify config bugs

--- WebSite/quasar.conf.js
+++ WebSite/quasar.conf.js
@@ -23,6 +23,7 @@ module.exports = configure(function (ctx) {
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
+      'notification'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
